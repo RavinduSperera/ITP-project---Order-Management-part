b1b345f6b7b3d41e55f2039ad72c913eab6046ad
RavinduSperera <it22064622@my.sliit.lk>
2024-04-06 11:09:40 +0530

    created front end foleder and setup for designing and removed unwanted folders and files. gonna start coding :)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-//import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-
-// import the compornents
-import CreateOrder from './components/createOrder';
-import ViewOrder from './components/viewOrder';
 
 function App() {
   return (
     <div className='App'>
-      <CreateOrder/>
-      <ViewOrder/>
+     
     </div>
   );
 }
